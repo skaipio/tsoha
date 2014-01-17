a5806f96b214831cf800f6c4399dd9ca3f62040c
Senni Kaipio <skaipio@cs.helsinki.fi>
2014-01-17 22:04:58 +0200

    Lisää harkkatyön esittelysivu ja projektin aloitussivu

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $aloitus = "Heippa hei";
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+
+ 
+ <!DOCTYPE HTML>
+ <html>
+     <body>Heippa hei</body>
+ </html>
+
