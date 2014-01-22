600a9d7bd68838cc70c9f5889de4910fc8adf8aa
Senni K Kaipio <skaipio@svm-26.cs.helsinki.fi>
2014-01-22 17:43:07 +0200

    lisää html demon alustava sivu

---
+++ html-demo/tyovuorolista.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="../css/bootstrap.css" rel="stylesheet">
+        <link href="../css/bootstrap-theme.css" rel="stylesheet">
+        <link href="../css/main.css" rel="stylesheet">
+    </head>
+    <body>
+        <ul class="nav nav-pills">
+            <li><a href="#"><a href="#">Omat työvuorot</a></li>
+            <li><a href="#"><a href="#">Työvuorolistat</a></li>
+            <li><a href="#"><a href="#">Henkilöstövahvuuskalenteri</a></li>
+            <li class="active"><a href="#">Työntekijät</a></li>          
+        </ul>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
