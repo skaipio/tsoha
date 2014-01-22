f27cc84dd399464a9b6eda662bfded4bfbea7deb
Senni K Kaipio <skaipio@svm-26.cs.helsinki.fi>
2014-01-22 18:01:59 +0200

    html-demon oletussivun muokkaus

---
+++ html-demo/tyovuorolistat.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="../css/bootstrap.css" rel="stylesheet">
+        <link href="../css/bootstrap-theme.css" rel="stylesheet">
+        <link href="../css/main.css" rel="stylesheet">
+    </head>
+    <body>
+        <ul class="nav nav-pills">
+            <li><a href="#">Omat työvuorot</a></li>
+            <li class="active">Työvuorolistat</a></li>
+            <li><a href="#">Henkilöstövahvuuskalenteri</a></li>
+            <li><a href="#">Työntekijät</a></li>          
+        </ul>
+        <div class="container">
+            <h1>Työvuorolistat</h1>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Viikonpäivä</th>
+                        <th>Päivämäärä</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><button type="button" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-cutlery"></span>Lisää työvuorolista</button></td>
+                    </tr>
+                    <tr>
+                        <td>Keskiviikko 22.1.2014</td>
+                    </tr>
+                    <tr>
+                        <td>Tiistai 21.1.2014</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
