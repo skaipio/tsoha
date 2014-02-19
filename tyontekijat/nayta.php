363e6b8923446345355fb50e807e27205fa862e9
Senni Kaipio <skaipio@cs.helsinki.fi>
2014-02-19 03:36:36 +0200

    Fix employee show page

--- tyontekijat/nayta.php
+++ tyontekijat/nayta.php
@@ -6,18 +6,15 @@ require "../libs/common.php";
 
 if (loggedInAsAdmin()) {
     setNavBarAsVisible(false);
-    $admin = $user->isAdmin();
-    if ($admin) {
-        $id = $_GET['id'];
-        if (isset($id)) {            
-            $employee = Employee::getEmployeeByID($id);
-            $prcategory = Personnelcategory::getPersonnelCategoryById($employee->getPersonnelCategoryID());
-            showView("views/showEmployee.php", array('admin'=>true, 'employee'=>$employee, 'personnelCategory'=>$prcategory));
-        }
-    } else {
-        setErrors(array("Sivu vaatii ylläpito-oikeudet."));
-        showOnlyTemplate();
+
+    $id = $_GET['id'];
+    if (isset($id)) {
+        $employee = Employee::getEmployeeByID($id);
+        $prcategory = Personnelcategory::getPersonnelCategoryById($employee->getPersonnelCategoryID());
+        showView("views/showEmployee.php", array('admin' => true, 'employee' => $employee, 'personnelCategory' => $prcategory));
     }
+    
 } else {
+    setErrors(array("Sivu vaatii ylläpito-oikeudet."));
     redirectTo('../kirjautuminen.php');
 }
--- views/showEmployee.php
+++ views/showEmployee.php
@@ -2,27 +2,33 @@
     <div class="col-lg-offset-2 col-lg-8">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h3 class="panel-title"><?php echo "$data->personnelcategory $data->firstname $data->lastname" ?></h3>
+                <h3 class="panel-title"><?php
+                    $personnelCategoryName = htmlspecialchars($data->personnelCategory->getName());
+                    $employee = $data->employee;
+                    $firstname = htmlspecialchars($employee->getFirstName());
+                    $lastname = htmlspecialchars($employee->getLastName());
+                    echo "$personnelCategoryName $firstname $lastname";
+                    ?></h3>
             </div>
             <div class="panel-body">
                 <div class="row">
                     <div class="col-lg-6">
                         <label class="col-lg-5">Henkilötunnus:</label>
-                        <span class="col-lg-6"><?php echo htmlspecialchars($data->ssn) ?></span>
+                        <span class="col-lg-6"><?php echoToPage($employee->getSSN()) ?></span>
                     </div>
                     <div class="col-lg-6">
                         <label class="col-lg-4">Osoite:</label>
-                        <span class="col-lg-8"><?php echo htmlspecialchars($data->address) ?></span>
+                        <span class="col-lg-8"><?php echoToPage($employee->getAddress()) ?></span>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-lg-6">
                         <label class="col-lg-5 control-label">Sähköposti:</label>
-                        <span class="col-lg-6"><?php echo htmlspecialchars($data->email) ?></span>
+                        <span class="col-lg-6"><?php echoToPage($employee->getEmail()) ?></span>
                     </div>
                     <div class="col-lg-6">
                         <label class="col-lg-4 control-label">Puhelin:</label>
-                        <span class="col-lg-8"><?php echo htmlspecialchars($data->phone) ?></span>
+                        <span class="col-lg-8"><?php echoToPage($employee->getPhone()) ?></span>
                     </div>
                 </div>
                 <div class="row"></div>
@@ -30,26 +36,27 @@
                     <div class="col-lg-6"></div>
                     <div class="col-lg-6">
                         <label class="col-lg-7">Max tunnit viikossa:</label>
-                        <span class="col-lg-3"><?php echo htmlspecialchars($data->maxhoursperweek) ?></span>
+                        <span class="col-lg-3"><?php echoToPage($employee->getMaxHoursPerWeek()) ?></span>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-lg-6">
                         <label class="col-lg-6">Ylläpito-oikeudet:</label>
-                        <?php $admin = ($data->admin) ? "On" : "Ei ole"; ?>
+                        <?php $admin = ($employee->isAdmin()) ? "On" : "Ei ole"; ?>
                         <span class="col-lg-5"><?php echo $admin ?></span>
                     </div>
                     <div class="col-lg-6">
                         <div class="form-group">
                             <label class="col-lg-7">Max tunnit päivässä</label>
-                            <span class="col-lg-3"><?php echo htmlspecialchars($data->maxhoursperday) ?></span>
+                            <span class="col-lg-3"><?php echoToPage($employee->getMaxHoursPerDay()) ?></span>
                         </div>
                     </div>
                 </div>
                 <div class="row" style="padding-top: 10px">
                     <div class="col-lg-6">
-                        <a href="<?php echo "muokkaa.php?id=$data->id" ?>"><button type="button" class="btn btn-default">Muokkaa</button></a> 
-                        <a href="<?php echo "poista.php?id=$data->id" ?>"><button type="button" class="btn btn-default">Poista työntekijä</button></a>
+                        <?php $id=$employee->getID();?>
+                        <a href="<?php echo "muokkaa.php?id=$id" ?>"><button type="button" class="btn btn-default">Muokkaa</button></a> 
+                        <a href="<?php echo "poista.php?id=$id" ?>"><button type="button" class="btn btn-default">Poista työntekijä</button></a>
                         <a href="index.php"><button class="btn btn-default" type="cancel">Takaisin</button>
                     </div>
                 </div>
